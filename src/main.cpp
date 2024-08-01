4f1bafb8c90140eeb60cdc619c6e5ed5aff279c9
root <root@localhost>
2024-08-01 13:24:53 +0530

    auto publish 1722498893472

---
+++ src/main.cpp
@@ -0,0 +1,68 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+
+
+int main()
+{   clock_t start_time =clock();
+    cout << "Hello World!" << endl;
+    
+    vector<int>v;
+    //v.push_back(1);
+    
+    cout<<v.capacity()<<endl;
+    v.push_back(1);
+    
+    cout<<v.capacity()<<endl;
+    v.push_back(1);
+    
+    cout<<v.capacity()<<endl;
+    v.push_back(1);
+    
+    cout<<v.capacity()<<endl;
+    v.push_back(1);
+    
+    cout<<v.capacity()<<endl;
+    v.push_back(1);
+    
+    cout<<v.capacity()<<endl;
+    v.push_back(1);
+    
+    cout<<v.capacity()<<endl;
+    v.push_back(1);
+    
+    cout<<v.capacity()<<endl;
+    v.push_back(1);
+    cout<<v.capacity()<<endl;
+    v.push_back(1);
+    
+    cout<<v.capacity()<<endl;
+    v.push_back(1);
+    
+    cout<<v.capacity()<<endl;
+    v.push_back(1);
+    
+    cout<<v.capacity()<<endl;
+    v.push_back(1);
+    
+    cout<<v.capacity()<<endl;
+    v.push_back(1);
+    
+    cout<<v.capacity()<<endl;
+    v.push_back(1);
+    
+    cout<<v.capacity()<<endl;
+    v.push_back(1);
+    
+    cout<<v.capacity()<<endl;
+    v.push_back(1);
+    
+    clock_t end_time=clock();
+  double timespend=  (double) (end_time-start_time)/CLOCKS_PER_SEC*1e6;
+    cout << "Time to run the code "<<timespend <<"microsecond"<< endl;
+    
+    
+    return 0;
+}
